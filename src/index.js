a275d4abb83b4bc4d9e1b3c82d9bce7bbdbdca3d
kai <b10223052@yuntech.org.tw>
2019-09-14 13:46:39 +0800

    init redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './Page/home';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxs from './reduxs';
+
+let store = createStore(
+	reduxs,
+	applyMiddleware(logger),
+);
 
 function App() {
 	return (
-		<Home/>
+		<Provider store={store}>
+			<Home/>
+		</Provider>
 	);
 }
 
